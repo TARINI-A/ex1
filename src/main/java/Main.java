ecf6d68a1d2937611298ec2505996e28691d8779
TARINI A <tariniarjunan9147@gmail.com>
2026-09-01 16:25:37 +0530

    Fix Java syntax error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    sudo apt install git   public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello! Maven Build Pipeline is working successfully.");
     }
 }
